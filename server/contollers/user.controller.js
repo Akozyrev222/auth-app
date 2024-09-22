5842fa09d64f917ebf4e3d104d3d65128150b9b8
andreykozyrev <akozyrev222@gmail.com>
2024-09-23 00:44:16 +0300

    update setup

--- server/contollers/user.controller.js
+++ server/contollers/user.controller.js
@@ -57,7 +57,7 @@ const authUser = async (req, res) => {
                     }, {new: true})
                     const name = user.name
                     const token = await jwt.sign({name}, 'jwt-secret-key', {expiresIn: '1d'})
-                    res.cookie('token', token, {sameSite: "none"})
+                    res.cookie('token', token)
                     return res.json({Status: "Success", user: updateUser, token: token})
                 } else {
                     return res.json({Error: 'Password not matched'})

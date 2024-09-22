f6359eb548816727d1d5e254075b44898ef791ed
andreykozyrev <akozyrev222@gmail.com>
2024-09-22 05:37:54 +0300

    deploy setup

--- api/index.js
+++ api/index.js
@@ -17,12 +17,6 @@ app.use(cookieParser());
 const MONGO_URI = 'mongodb+srv://root:NHRRa1Z3RcdxNUbV@auth.qld6y.mongodb.net/Users?retryWrites=true&w=majority&appName=Auth';
 
 mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
 
 
 app.use('/', userRoute)

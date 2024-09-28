4ade85511c24f9e44135ad48dad46ac6a5ecf3ed
andreykozyrev <akozyrev222@gmail.com>
2024-09-28 21:20:28 +0300

    change to prod api url

--- frontend/src/commonFunctions/AJAX/AJAX.js
+++ frontend/src/commonFunctions/AJAX/AJAX.js
@@ -4,7 +4,7 @@ import {BASE_API, BASE_URL_DEV, BASE_URL_PROD} from "../constants.js";
 export const AJAX = (params) => {
     const {method, data, url} = params
     return axios({
-        url: `/${BASE_API}${url}`,
+        url: `${BASE_URL_PROD}${BASE_API}${url}`,
         method: method,
         data: data,
         withCredentials: true
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
+    /*server: {
         proxy: {
             //'/api': 'http://localhost:3000'
             '/api': 'https://auth-app-virid-eight.vercel.app/'
         }
-    },
+    },*/
     plugins: [react()],
 })

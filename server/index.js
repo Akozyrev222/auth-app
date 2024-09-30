79b78a01f10c7768b5fb87852fbbf79d8cac1cf5
andreykozyrev <akozyrev222@gmail.com>
2024-09-30 04:13:12 +0300

    update user verify

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["https://auth-front-wheat.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
